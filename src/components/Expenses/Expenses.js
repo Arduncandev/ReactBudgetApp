c9376a2976fd9ffe994b1e2e97a6d32389c1ab2b
Alex Duncan <alexduncan@ibm.com>
2023-02-22 11:17:49 -0600

    first commit

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import './Expenses.css'
+import Card from '../UI/Card'
+import ExpensesFilter from './ExpensesFilter'
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
+
+function Expenses(props) {
+    const [filteredYear, setEnteredYear] = useState('2020');
+
+    const newFilterHandler = (newFilterYear) => {
+        setEnteredYear(newFilterYear);
+    }
+
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    })
+
+    return (
+        <div>
+            <Card className='expenses'>
+                <ExpensesFilter selected={filteredYear} onNewFilter={newFilterHandler} />
+                <ExpensesChart expenses={filteredExpenses} />
+                <ExpensesList items={filteredExpenses}/>
+            </Card>
+        </div>
+    );
+}
+
+export default Expenses;
